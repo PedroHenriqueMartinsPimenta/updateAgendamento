fde91301a6938d1b0d99f0bcfc48361306bff1ac
PedroHenriqueMartinsPimenta <pedrohenrique234322@gmail.com>
2019-06-18 19:51:52 -0300

    reCapthca

---
+++ adm_files/html/init/acesso restrito/enter/enter/enter/init_pesquisa.php
@@ -0,0 +1,13 @@
+<?php 
+session_start();
+	if (!isset($_SESSION['CPF'])) {
+		
+		include_once('../../../conexao.php');
+		$sql = "DELETE FROM PESQUISA WHERE CODIGO != 0";
+		$query = mysqli_query($con, $sql);
+
+	}else{
+		echo "<b>Você não pode acessar este arquivo!<?b>";
+	}
+
+?>

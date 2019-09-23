c7ae1677fdec8977f2a779c42977388bbe1c1b99
PedroHenriqueMartinsPimenta <pedrohenrique234322@gmail.com>
2019-09-23 09:33:57 -0300

    Cancelar agendamento

--- php/delete agendamento.php
+++ php/delete agendamento.php
@@ -1,51 +1,33 @@
 <?php 
+
 include_once("conexao.php");
 session_start();
-$codigo = $_GET['codigo'];
-$dia = date('d');
-$mes = date('m');
-$ano = date('Y');
-$sql = "SELECT * FROM RESERVA WHERE CODIGO = $codigo AND DAY(DATA_ULTILIZAR) > $dia AND MONTH(DATA_ULTILIZAR) >= $mes AND YEAR(DATA_ULTILIZAR) >= $ano";
-$query = mysqli_query($con, $sql);
-if (mysqli_num_rows($query) > 0) {
 
-$sql="DELETE FROM RESERVA WHERE CODIGO = $codigo AND DAY(DATA_ULTILIZAR) > $dia AND MONTH(DATA_ULTILIZAR) >= $mes AND YEAR(DATA_ULTILIZAR) >= $ano";
+$codigo = $_GET['codigo'];
+
+$sql="DELETE FROM RESERVA WHERE CODIGO = $codigo";
 
 $query = mysqli_query($con,$sql);
+
 if ($query) {
-	if($_SESSION['PERMISSAO'] == 1){
+    if($_SESSION['PERMISSAO'] == 1){
 ?>
 <script>
-alert("Agendamento deletado com sucesso!");
-window.location.href = "../adm_files/html/reservas.php";
-</script>
-<?php
-}else{
-    ?>
-    <script>
     alert("Agendamento deletado com sucesso!");
-    window.location.href = "../user_files/html/reservas.php";
-    </script>
-<?php
-}
-}else{
-	echo mysqli_error($con);
-}
-}else{
-	if($_SESSION['PERMISSAO'] == 1){
-?>
-<script>
-alert("Agendamento não pode ser mais cancelado! \n Somente com um dia de antecedencia!");
-window.location.href = "../adm_files/html/reservas.php";
+    window.location.href = "../adm_files/html/reservas.php";
 </script>
 <?php
 }else{
     ?>
     <script>
-    alert("Agendamento não pode ser mais cancelado! \n Somente com um dia de antecedencia!");
-    window.location.href = "../user_files/html/reservas.php";
+        alert("Agendamento deletado com sucesso!");
+        window.location.href = "../user_files/html/reservas.php";
     </script>
 <?php
 }
+
+}else{
+   echo mysqli_error($con);
 }
+
 ?>
